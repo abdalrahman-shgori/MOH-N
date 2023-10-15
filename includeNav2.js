02b3134c52ffcbb14dde26daaf432d55ebd39ad9
abd <abedoocool@gmail.com>
2023-10-15 03:15:22 +0300

    first commit

--- includeNav2.js
+++ includeNav2.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const head = document.head;
 
     // Fetch the navigation bar from nav.html
-    fetch("../../components/navbar/nav.html")
+    fetch("./pages/nav.html")
         .then(response => response.text())
         .then(navHTML => {
             document.getElementById("navbar-container").innerHTML = navHTML;
 
             // Include the language.js script after the navigation bar is loaded
             const script = document.createElement("script");
-            script.src = "../../components/languageSwitcher/language.js";
+            script.src = "./js/language.js";
             head.appendChild(script);
             
         })
