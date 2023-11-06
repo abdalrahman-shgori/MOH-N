762e4dad2419a8306efc3d789e61e43a9aed3fe6
abd <abedoocool@gmail.com>
2023-11-06 20:03:50 +0300

    first commit

---
+++ js/footerLang.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const head = document.head;
+
+    fetch("../js/language.js")
+        .then(response => response.text())
+        .then(navHTML => {
+            // document.getElementById("navbar-container").innerHTML = navHTML;
+
+            const script = document.createElement("script");
+            script.src = "../js/language.js";
+            head.appendChild(script);
+
+        })
+        .catch(error => {
+            console.error("Error fetching navigation bar:", error);
+        });
+
+        fetch("../pages/footer.html")
+        .then(response => response.text())
+        .then(navHTML => {
+            document.getElementById("footer").innerHTML = navHTML;
+
+            const script = document.createElement("script");
+            script.src = "../js/language.js";
+            head.appendChild(script);
+            console.log("abd")
+
+        })
+        .catch(error => {
+            console.error("Error fetching navigation bar:", error);
+        });
+
+
+});
+
