989f3db1c63f089d9783b4deff29f2eba0104db9
abd <abedoocool@gmail.com>
2023-11-06 21:44:34 +0300

    first commit

--- js/includeNav.js
+++ js/includeNav.js
@@ -2,18 +2,22 @@ document.addEventListener("DOMContentLoaded", function () {
     const head = document.head;
 
     fetch("../pages/nav.html")
-        .then(response => response.text())
-        .then(navHTML => {
-            document.getElementById("navbar-container").innerHTML = navHTML;
+    .then(response => response.text())
+    .then(navHTML => {
+        document.getElementById("navbar-container").innerHTML = navHTML;
 
-            const script = document.createElement("script");
-            script.src = "../js/language.js";
-            head.appendChild(script);
+        // Create a script element for language.js
+        const script = document.createElement("script");
+        script.src = "../js/language.js";
+        
+        // Append the script to the head element
+        const head = document.querySelector('head');
+        head.appendChild(script);
 
-        })
-        .catch(error => {
-            console.error("Error fetching navigation bar:", error);
-        });
+    })
+    .catch(error => {
+        console.error("Error fetching navigation bar:", error);
+    });
         
 
         fetch("../pages/footer.html")
