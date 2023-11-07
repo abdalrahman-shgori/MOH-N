d4fa70259f1b93c07fefa86131d6d4ee65241bb9
abd <abedoocool@gmail.com>
2023-11-07 18:38:27 +0300

    first commit

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,5 @@
 function myFunction(event, language) {
   document.getElementById("current-language").innerText = event.target.innerText;
   document.getElementById("current-language").setAttribute("data-lang", language);
+  
 }
