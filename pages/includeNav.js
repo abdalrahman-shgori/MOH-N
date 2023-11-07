67789e9e8937f8b50929090e9190d55e8ac55612
abd <abedoocool@gmail.com>
2023-11-07 04:38:25 +0300

    first commit

--- pages/includeNav.js
+++ pages/includeNav.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const head = document.head;
 
-    fetch("./nav.html")
+    fetch("nav.html")
         .then(response => response.text())
         .then(navHTML => {
             document.getElementById("navbar-container").innerHTML = navHTML;
             const script = document.createElement("script");
-            script.src = "./language.js";
+            script.src = "language.js";
             head.appendChild(script);
 
         })
@@ -16,13 +16,13 @@ document.addEventListener("DOMContentLoaded", function () {
         
 
 
-        fetch("./footer.html")
+        fetch("footer.html")
         .then(response => response.text())
         .then(navHTML => {
             document.getElementById("footer").innerHTML = navHTML;
 
             const script = document.createElement("script");
-            script.src = "./js/language.js";
+            script.src = "language.js";
             head.appendChild(script);
         })
         .catch(error => {
